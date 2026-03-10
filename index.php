8b102c790af9b8b134b8dd9cdab1574ad178eeec
Johanneshwp <jamieza61989@gmail.com>
2026-03-10 11:50:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Lab 2: Docker Container</h1>";
+echo "Name: Noppavit podschwadek <br>";
+echo "Student ID: 2651032341169 <br>";
+echo "Major: Information Technology <br>";
+echo "Project: Cloud Computing Lab 2 <br>";
+?>
